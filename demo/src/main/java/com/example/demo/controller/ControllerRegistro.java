d5f5bb5eed8f1bcd37a765d54fa0675a362442e1
Elconme <201910743@alu.comillas.edu>
2022-04-01 20:25:02 +0200

    p

---
+++ demo/src/main/java/com/example/demo/controller/ControllerRegistro.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.TablaRegistro;
+import com.example.demo.service.ServiceRegistro;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/registro")
+public class MyController {
+
+    @Autowired
+    private ServiceRegistro servicioRegistro;
+
+    @GetMapping("/elements")
+    public ResponseEntity<List<TablaRegistro>> getElements() {
+
+        var elements = servicioRegistro.getElements();
+
+        return ResponseEntity.ok().body(elements);
+    }
+
+}
---
+++ demo/src/main/resources/registro_datos.sql
@@ -0,0 +1,4 @@
+-- Insertando algunos registros para que la base de datos no este vacia
+
+INSERT INTO TABLA_REGISTRO(NIF, NOMBRE, APELLIDO1, APELLIDO2, CORREO, CUMPLEANOS) VALUES ('09233969L','Sergio','Cuenca','Núñez','sc@gmail.com','2000-01-15');
+INSERT INTO TABLA_REGISTRO(NIF, NOMBRE, APELLIDO1, APELLIDO2, CORREO, CUMPLEANOS) VALUES ('09234356L','Ana','Alvarez','Perez','a@hotmail.com','2000-01-15');
