51f83b1d2840d75f6d3f0c46028d0bfd36a4970c
Elconme <92789004+Elconme@users.noreply.github.com>
2022-05-12 12:46:04 +0200

    Update APP_ROLES.java

--- src/main/java/com/example/demo/service/APP_ROLES.java
+++ src/main/java/com/example/demo/service/APP_ROLES.java
@@ -2,28 +2,28 @@ package com.example.demo.service;
 
 public enum APP_ROLES {
     ROLE_USER("ROLE_USER"),
-        ROLE_ADMIN("ROLE_ADMIN");
+    ROLE_ADMIN("ROLE_ADMIN");
 
-        private final String abbreviation;
+    private final String abbreviation;
 
-        // Reverse-lookup map for getting a day from an abbreviation
-        private static final Map<String, APP_ROLES> lookup = new HashMap<String, APP_ROLES>();
+    // Reverse-lookup map for getting a day from an abbreviation
+    private static final Map<String, APP_ROLES> lookup = new HashMap<String, APP_ROLES>();
 
-        static {
-            for (APP_ROLES d : APP_ROLES.values()) {
-                lookup.put(d.getAbbreviation(), d);
-            }
+    static {
+        for (APP_ROLES d : APP_ROLES.values()) {
+            lookup.put(d.getAbbreviation(), d);
         }
+    }
 
-        private APP_ROLES(String abbreviation) {
-            this.abbreviation = abbreviation;
-        }
+    private APP_ROLES(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
 
-        public String getAbbreviation() {
-            return abbreviation;
-        }
+    public String getAbbreviation() {
+        return abbreviation;
+    }
 
-        public static APP_ROLES get(String abbreviation) {
-            return lookup.get(abbreviation);
-        }
+    public static APP_ROLES get(String abbreviation) {
+        return lookup.get(abbreviation);
+    }
 }
