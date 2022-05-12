db67580337cd69fa53aae500ddce7bfd3297bad8
Sergio Cuenca Núñez <sergiocuencanunez@MacBook-Pro-de-Sergio.local>
2022-05-12 19:06:58 +0200

    Práctica Final

--- src/main/java/com/example/demo/service/LoginService.java
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -6,11 +6,7 @@ import com.example.demo.model.Usuario;
 public interface LoginService {
 
   LoginServiceResult inicioSesionDeUsuario(Usuario usuario);
-<<<<<<< HEAD
-  APP_ROLES getRole(String accessToken);
-=======
   ROLES getRole(String accessToken);
->>>>>>> 66c03206cd986c9289f8fd205852ab1c0c21b01a
   LoginServiceResult registroUsuario(Usuario usuario);
   LoginServiceResult mensajeContacto(Contacto contacto);
   LoginServiceResult checkInContacto(Contacto contacto);
